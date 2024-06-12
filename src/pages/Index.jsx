cac229510327b1bc7c777f15a5139c3a809a91d8
Luiz Henrique da Silva Araújo <thuco.jandinopolis@gmail.com>
2024-06-12 19:19:48 -0300

    add search

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,7 @@
 import { faMagnifyingGlass, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React  from 'react';
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import api from '../service/api';
 import MainLogo from '../shared/components/MainLogo';
@@ -13,13 +14,30 @@ const fetchRestaurants = async () => {
 
 const Home = () => {
     const { data: restaurants, isLoading } = useQuery('restaurants', fetchRestaurants);
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearchChange = (event) => {
+        setSearchText(event.target.value);
+    };
+
+    const filteredRestaurants = !isLoading && restaurants.filter((restaurant) =>
+        restaurant.name.toLowerCase().includes(searchText.toLowerCase())
+    );
 
     return (
         <div id='home'>
             <div className="main-header">
                 <MainLogo />
                 <div class="search-div input-group mb-3">
-                    <input type="text" class="form-control" placeholder="Pesquisar restaurante" aria-label="Recipient's username" aria-describedby="button-addon2" />
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Pesquisar restaurante"
+                        aria-label="Recipient's username"
+                        aria-describedby="button-addon2"
+                        value={searchText}
+                        onChange={handleSearchChange}
+                    />
                     <button class="btn btn-outline-secondary" type="button" id="button-addon2"><FontAwesomeIcon className='text-dark' icon={faMagnifyingGlass} /></button>
                 </div>
             </div>
@@ -29,22 +47,26 @@ const Home = () => {
                     <FontAwesomeIcon icon={faUtensils} size='2x' />
                     <h2>Restaurantes disponíveis</h2>
                 </div>
-                {!isLoading ?
-                    restaurants.map(restaurant => (
-                        <div key={restaurant.id} className="restaurant-card" onClick={() => window.location.href = `/${restaurant.username}`}>
-                            <h3 className='restaurant-name'>{restaurant.name}</h3>
-                            <img className='restaurant-logo' src={restaurant.logo} alt={restaurant.name} />
-                        </div>
-                    )) 
-                    :
+                {isLoading ? (
                     <div style={{marginTop: '50px'}}>
-                        <div class="snippet" data-title="dot-flashing">
-                            <div class="stage">
-                                <div class="dot-flashing"></div>
-                            </div>
+                    <div class="snippet" data-title="dot-flashing">
+                        <div class="stage">
+                        <div class="dot-flashing"></div>
                         </div>
                     </div>
-                }
+                    </div>
+                ) : (
+                    restaurants.map(restaurant => (
+                    <div
+                        key={restaurant.id}
+                        className={`restaurant-card ${searchText !== '' && !restaurant.name.toLowerCase().includes(searchText.toLowerCase()) ? 'd-none' : ''}`}
+                        onClick={() => window.location.href = `/${restaurant.username}`}
+                    >
+                        <h3 className='restaurant-name'>{restaurant.name}</h3>
+                        <img className='restaurant-logo' src={restaurant.logo} alt={restaurant.name} />
+                    </div>
+                    ))
+                )}
             </div>
         </div>
     );
